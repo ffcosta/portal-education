cc2228645cd4d97e1faa1d6931ae4b6d2a85d6f6
Fernando Ferreira da Costa <ffcosta@MacBook-Pro-de-Fernando.local>
2019-06-16 16:09:54 -0300

    Updates

---
+++ precache-manifest.28428607bf4b58800e39fc251e6d09e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bcdf7ee78d9f21ea71d22ebe9bd586c",
+    "url": "/portal-education/index.html"
+  },
+  {
+    "revision": "3fc4d8ab808a7b9d697b",
+    "url": "/portal-education/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portal-education/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3fc4d8ab808a7b9d697b",
+    "url": "/portal-education/static/js/main.564633aa.chunk.js"
+  },
+  {
+    "revision": "53fcb55c07219b22fa15",
+    "url": "/portal-education/static/js/runtime~main.62dfb527.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portal-education/static/media/logo.5d5d9eef.svg"
+  }
+]);
